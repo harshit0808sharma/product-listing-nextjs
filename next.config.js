eff1849e4ea53a6cd2a2336137f619123392c58c
sachin sharma <harshit0808sharma@gmail.com>
2025-06-14 16:52:26 +0530

    some changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     domains: ['fakestoreapi.com'],
   },
-}
+  output: 'export', // ✅ Required for static export in Next.js 13+
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     domains: ['fakestoreapi.com'],
+//   },
+// }
+
+// module.exports = nextConfig
